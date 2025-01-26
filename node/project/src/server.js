a5ef9a678cd935420dd60466a3f16f50e49b0541
Lucas Cardoso de Medeiros <lucascarmed@gmail.com>
2025-01-25 23:36:49 -0300

    Node 6

--- node/project/src/server.js
+++ node/project/src/server.js
@@ -1,7 +1,13 @@
 const port = 3003;
+const database = require("./database");
 const express = require("express");
 const app = express();
-
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 
 app.get("/products", (req, res, next) => {
   console.log("Middleware 1...");
@@ -9,7 +15,34 @@ app.get("/products", (req, res, next) => {
 });
 
 app.get("/products", (req, res, next) => {
-  res.send({ name: "Notebook", price: 1249.99 });
+  res.send(database.getProducts());
+});
+
+app.get("/product/:id", (req, res, next) => {
+  const product = database.getProduct(req.params.id) || {};
+  res.send(product);
+});
+
+app.post("/product", (req, res, next) => {
+  const product = database.saveProduct({
+    name: req.body.name,
+    price: req.body.price
+  });
+  res.send(product);
+});
+
+app.put("/product/:id", (req, res, next) => {
+  const product = database.saveProduct({
+    id: req.params.id,
+    name: req.body.name,
+    price: req.body.price
+  });
+  res.send(product);
+});
+
+app.delete("/product/:id", (req, res, next) => {
+  const product = database.deleteProduct(req.params.id);
+  res.send(product);
 });
 
 app.listen(port, () => {
---
+++ node/schedule.js
@@ -0,0 +1,19 @@
+const schedule = require("node-schedule");
+
+const task1 = schedule.scheduleJob("* * * * *", function() {
+  console.log("Runing task 1!", new Date().getSeconds());
+});
+
+setTimeout(function() {
+  task1.cancel();
+  console.log("Task 1 cancelled");
+}, 20000);
+
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [new schedule.Range(1, 7)];
+rule.hour = 23;
+rule.second = 30;
+
+const task2 = schedule.scheduleJob(rule, function() {
+  console.log("Runing task 2!", new Date().getSeconds());
+});
